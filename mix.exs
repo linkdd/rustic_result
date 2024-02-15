a4f023891680bb5455098fc3c7d1d3d6123d7daa
David Delassus <david.jose.delassus@gmail.com>
2024-02-15 09:37:46 +0100

    :bookmark: v0.7.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rustic.Result.MixProject do
   def project do
     [
       app: :rustic_result,
-      version: "0.6.1",
+      version: "0.7.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
